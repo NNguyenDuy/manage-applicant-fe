18df87a00852a05fa78d2fc821f55bd1fe953eb8
NguyenDuy <nguyennduy1708@gmail.com>
2024-08-18 23:37:38 +0700

    feat: header

--- src/app/routes.ts
+++
@@ -1,9 +0,0 @@
-const ROOTS = {
-  HOME: '/',
-  BOOK: '/book',
-}
-
-export const PATHS = {
-  HOME: ROOTS.HOME,
-  BOOK: ROOTS.BOOK,
-}
--- src/shared/components/header.tsx
+++ src/shared/components/header.tsx
@@ -1,15 +1,47 @@
-const Header = () => {
+'use client'
+import Image from 'next/image'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { PATHS, ROOTS } from './routes'
+export const Header = () => {
+  const pathname = usePathname()
+
   return (
-    <div className="bg-[#101010] rounded-lg flex items-center justify-between p-4">
-      <div></div>
-      <div className="flex gap-10 items-center font-bold">
-        <div className="text-gray-300">Đăng ký</div>
-        <div className="bg-white rounded-3xl p-2 px-4 text-black">
+    <header className="flex justify-between h-20 items-center px-8">
+      <Link href={ROOTS.HOME}>
+        <Image
+          src="/assets/images/logo.png"
+          alt="logo"
+          width={100}
+          height={100}
+          className="rounded-full"
+        />
+      </Link>
+      <nav className="flex gap-10 font-semibold text-secondary">
+        {PATHS.HOME.map((item) => (
+          <Link
+            key={item.path}
+            href={item.path}
+            className={pathname === item.path ? 'text-primary' : ''}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </nav>
+      <div className="flex gap-10">
+        <Link
+          href={ROOTS.LOGIN}
+          className="border text-primary rounded-sm border-primary px-4 py-2 font-bold"
+        >
           Đăng nhập
-        </div>
+        </Link>
+        <Link
+          href={ROOTS.REGISTER}
+          className="text-white rounded-sm bg-primary px-4 py-2 font-bold"
+        >
+          Đăng ký
+        </Link>
       </div>
-    </div>
+    </header>
   )
 }
-
-export default Header
