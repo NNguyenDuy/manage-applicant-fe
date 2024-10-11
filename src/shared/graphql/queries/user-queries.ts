f4479e26cf67d6afc7b774dc5a8f8aaf050fa1ec
NNguyenDuy <nguyennduy1708@gmail.com>
2024-10-11 15:59:07 +0700

    Feat(user): adđ location

--- src/shared/graphql/queries/user-queries.ts
+++ src/shared/graphql/queries/user-queries.ts
@@ -2,41 +2,49 @@ import { gql } from '@apollo/client'
 
 export const GET_INFO_USER = gql`
   query GetInfoUser {
-  getInfoUser {
-    _id
-    fullName
-    email
-    role
-    candidateProfile {
+    getInfoUser {
       _id
-      userId
-      resume {
-        cvLinks
-        skills {
-          name
-          experience
+      fullName
+      email
+      role
+      candidateProfile {
+        _id
+        userId
+        resume {
+          cvLinks
+          skills {
+            name
+            experience
+          }
         }
       }
-      applications {
+      company {
         _id
-        jobId
-        candidateProfileId
-        status
-        appliedAt
-      }
-    }
-    company {
-      _id
-      name
-      ownerId
-      locationId
-      jobs {
-        _id
-        title
-        description
-        jobType {
+        name
+        ownerId
+        locationId
+        jobs {
           _id
-          type
+          title
+          description
+          jobType {
+            _id
+            type
+          }
+          location {
+            _id
+            address
+            city
+            country
+          }
+          categoryIds {
+            _id
+            name
+          }
+          candidates {
+            _id
+            userId
+          }
         }
         location {
           _id
@@ -44,16 +52,7 @@ export const GET_INFO_USER = gql`
           city
           country
         }
-        categoryIds {
-          _id
-          name
-        }
-        candidates {
-          _id
-          userId
-        }
       }
     }
   }
-}
 `
