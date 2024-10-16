cc52d2dd32b1cde000dcdcdcfc0185f642b1c667
NguyenDuy <nguyennduy1708@gmail.com>
2024-10-16 22:18:10 +0700

    Done task5

--- src/app/(main)/(user)/recruiter/profile-company/page.tsx
+++ src/app/(main)/(user)/recruiter/profile-company/page.tsx
@@ -2,16 +2,11 @@
 
 import React from 'react'
 import ManageInfoCompanies from './manage-infor-companies'
-import CompanyList from './company-list'
 
 const ProfileCompany = () => {
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Quản lý công ty</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <CompanyList />
-        <ManageInfoCompanies />
-      </div>
+    <div>
+      <ManageInfoCompanies />
     </div>
   )
 }
