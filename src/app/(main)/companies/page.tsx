af6357c0990ddfa1f83ebc491c6fa5a4733ced3c
xuantruong2k33 <nguyenxuantruong19122003@gmail.com>
2024-12-05 07:50:18 +0700

    code companies

--- src/app/(main)/companies/page.tsx
+++ src/app/(main)/companies/page.tsx
@@ -1,4 +1,32 @@
+"use client";
+
+import React, { useState } from 'react';
+import { useQuery } from '@apollo/client';
+import { GET_ALL_COMPANIES } from '#/shared/graphql/queries/company-queries';
+import CompanyList from './list-companies';
+
 const Company = () => {
-  return <div>Danh sách các công ty</div>
-}
-export default Company
+  const [filters] = useState({
+    Jtitle: '',
+    Jlocation: '',
+    JCategory: '',
+  });
+
+  const { data, loading, error } = useQuery(GET_ALL_COMPANIES);
+
+  if (loading) {
+    return <p>Đang tải...</p>;
+  }
+
+  if (error) {
+    return <p>Đã xảy ra lỗi: {error.message}</p>;
+  }
+
+  return (
+    <div>
+      <CompanyList companies={data?.getAllCompanies || []} />
+    </div>
+  );
+};
+
+export default Company;
--- src/shared/graphql/queries/company-queries.ts
+++ src/shared/graphql/queries/company-queries.ts
@@ -1,23 +1,56 @@
 import { gql } from '@apollo/client'
 
 export const GET_COMPANY = gql`
-  query GetCompany($id: ID!) {
-    getCompany(id: $id) {
-      _id
-      name
-      locationId
-    }
-  }
-`
-
-export const GET_ALL_COMPANIES = gql`
-  query GetAllCompanies {
-    getAllCompanies {
-      _id
+  query GetCompanyById($companyId: ID!) {
+    getCompanyById(companyId: $companyId) {
+      id
       name
       location {
         address
       }
     }
   }
-`
+`;
+
+
+export const GET_ALL_COMPANIES = gql`
+  query GetAllCompanies {
+    getAllCompanies {
+      id
+      name
+      description
+      size
+      field
+      locationId
+      isDel
+        location {
+      _id
+      address
+      city
+      country
+      isDel
+    }
+    }
+  }
+`;
+
+export const GET_COMPANY_BY_ID  = gql`
+query GetCompanyById($companyId: ID!) {
+  getCompanyById(companyId: $companyId) {
+    id
+    name
+    description
+    size
+    field
+    locationId
+    isDel
+    location {
+      _id
+      address
+      city
+      country
+      isDel
+    }
+  }
+}
+`;
